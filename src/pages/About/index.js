8869aea448ba8674b7829919c5a6a4490591d0d4
Dana Oira <danaoira@live.com>
2022-06-16 15:33:25 -0700

    Add Venn diagram SVG with intersect clipPath

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react'
 import { links } from '../../utils'
 import {
   Grid2Col,
--- src/pages/SpiderGraph/index.js
+++ src/pages/SpiderGraph/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from 'react'
-import ReactDOM from 'react-dom'
 import styled from 'styled-components'
 import { theme } from '../../utils'
 
@@ -37,17 +36,51 @@ const SpiderGraph = () => {
       if (!ref || !ref.current) return
       const computed = getComputedStyle(ref.current)
       console.log(computed)
-      setWidth(computed.width)
-      setHeight(computed.height)
+      setWidth(parseFloat(computed.width))
+      setHeight(parseFloat(computed.height))
     },
     [ref]
   )
 
-  console.log({ width, height })
   return (
     <Grid ref={ref}>
-      <svg width={width} height={height}>
+      <svg width={width} height={width / 2}>
         <rect width="100%" height="100%" fill={theme.color.pink} />
+        <g transform={`translate(${width * 0.04}, 0)`}>
+          {/* <g transform={`translate(0, 0)`}> */}
+          <circle
+            cx={width / 4}
+            cy={width / 4}
+            r={width / 4}
+            fill="rgba(255, 255, 255, 0.75)"
+            stroke={theme.color.white}
+            strokeWidth="2"
+          />
+          <circle
+            cx={width - width / 3}
+            cy={width / 4}
+            r={width / 4}
+            fill="rgba(255, 255, 255, 0.75)"
+            stroke={theme.color.white}
+            strokeWidth="2"
+          />
+          <g>
+            <clipPath id="clip1">
+              <circle cx={width / 4} cy={width / 4} r={width / 4} />
+            </clipPath>
+            <clipPath id="clip2" clip-path="url('#clip1')">
+              <circle cx={width - width / 3} cy={width / 4} r={width / 4} />
+            </clipPath>
+            <rect
+              clip-path="url('#clip2')"
+              x="0"
+              y="0"
+              width="100%"
+              height="100%"
+              fill={theme.color.pink}
+            />
+          </g>
+        </g>
       </svg>
     </Grid>
   )
