966dd5dfb6819fc926a6555fc879c44f9e786600
Dana Oira <danaoira@live.com>
2022-06-17 00:28:00 -0700

    Fix alignment issues on Venn diagram

--- src/pages/CareerProgression/index.js
+++ src/pages/CareerProgression/index.js
@@ -70,7 +70,7 @@ const SpiderChart = () => {
       const parsedWidth = parseFloat(computed.width)
       const parsedHeight = parseFloat(computed.height)
       setWidth(parsedWidth)
-      setHeight(Math.min(parsedHeight, parsedWidth / 2))
+      setHeight(Math.min(parsedHeight, height))
       if (width / 2 < radius) setRadius(width / 4)
     },
     [ref]
@@ -83,137 +83,140 @@ const SpiderChart = () => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <rect width="100%" height="100%" fill={theme.color.pink} />
-        <g transform={`translate(${width / 2}, 64)`}>
-          <text
-            x="0"
-            y="0"
-            style={{
-              fontFamily: theme.type.accent.fontFamily,
-              fontSize: theme.type.fontSize[6],
-              fill: theme.color.black,
-              textAnchor: 'middle',
-            }}
-          >
-            The Design Technology Skillset
-          </text>
-          <text
-            x="0"
-            y="32"
-            style={{
-              fontFamily: theme.type.accent.fontFamily,
-              fontSize: theme.type.fontSize[3],
-              fill: theme.color.black,
-              textAnchor: 'middle',
-              fontStyle: 'italic',
-            }}
-          >
-            by{' '}
-            <a
-              href="http://ericknudtson.com/ux-design-technologist.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Eric Knudtson
-            </a>
-          </text>
-        </g>
-        <g transform={`translate(${radius * 0.75}, 96)`}>
-          <g>
-            <circle
-              cx={radius}
-              cy={radius}
-              r={radius}
-              fill="rgba(255, 255, 255, 0.75)"
-              stroke={theme.color.white}
-              strokeWidth="2"
-            />
+        <g transform={`translate(${(width - radius * 3.6) / 2}, 0)`}>
+          <g transform={`translate(${radius * 1.8}, 64)`}>
             <text
-              x={radius}
-              y={radius * 0.5}
+              x="0"
+              y="0"
               style={{
                 fontFamily: theme.type.accent.fontFamily,
-                fontSize: theme.type.fontSize[4],
+                fontSize: theme.type.fontSize[6],
                 fill: theme.color.black,
                 textAnchor: 'middle',
               }}
             >
-              {vennData.uxDesign.title}
+              The Design Technology Skillset
             </text>
-            {vennData.uxDesign.skills.map((d, i) => (
-              <text
-                key={d}
-                x={radius}
-                y={radius * 0.65 + i * parseFloat(theme.type.lineHeight[4])}
-                style={{
-                  fontFamily: theme.type.default.fontFamily,
-                  fontSize: theme.type.fontSize[2],
-                  fill: theme.color.black,
-                  textAnchor: 'middle',
-                }}
-              >
-                {d}
-              </text>
-            ))}
-          </g>
-
-          <g transform={`translate(${radius * 1.6}, 0)`}>
-            <circle
-              cx={radius}
-              cy={radius}
-              r={radius}
-              fill="rgba(255, 255, 255, 0.75)"
-              stroke={theme.color.white}
-              strokeWidth="2"
-            />
             <text
-              x={radius}
-              y={radius * 0.5}
+              x="0"
+              y="32"
               style={{
                 fontFamily: theme.type.accent.fontFamily,
-                fontSize: theme.type.fontSize[4],
+                fontSize: theme.type.fontSize[3],
                 fill: theme.color.black,
                 textAnchor: 'middle',
+                fontStyle: 'italic',
               }}
             >
-              {vennData.digitalTechnology.title}
-            </text>
-            {vennData.digitalTechnology.skills.map((d, i) => (
-              <text
-                key={d}
-                x={radius}
-                y={radius * 0.65 + i * parseFloat(theme.type.lineHeight[4])}
-                style={{
-                  fontFamily: theme.type.default.fontFamily,
-                  fontSize: theme.type.fontSize[2],
-                  fill: theme.color.black,
-                  textAnchor: 'middle',
-                }}
+              by{' '}
+              <a
+                href="http://ericknudtson.com/ux-design-technologist.html"
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                {d}
-              </text>
-            ))}
+                Eric Knudtson
+              </a>
+            </text>
           </g>
 
-          <g>
-            <clipPath id="set1">
-              <circle cx={radius} cy={radius} r={255} />
-            </clipPath>
-            <clipPath id="set2" clipPath="url('#set1')">
+          <g transform={`translate(0, 96)`}>
+            <g>
               <circle
                 cx={radius}
                 cy={radius}
-                r={255}
-                transform={`translate(${radius * 1.6}, 0)`}
+                r={radius}
+                fill="rgba(255, 255, 255, 0.75)"
+                stroke={theme.color.white}
+                strokeWidth="2"
               />
-            </clipPath>
-            <rect
-              clipPath="url('#set2')"
-              x="0"
-              y="0"
-              width="100%"
-              height="100%"
-              fill={theme.color.pink}
-            />
+              <text
+                x={radius}
+                y={radius * 0.5}
+                style={{
+                  fontFamily: theme.type.accent.fontFamily,
+                  fontSize: theme.type.fontSize[4],
+                  fill: theme.color.black,
+                  textAnchor: 'middle',
+                }}
+              >
+                {vennData.uxDesign.title}
+              </text>
+              {vennData.uxDesign.skills.map((d, i) => (
+                <text
+                  key={d}
+                  x={radius}
+                  y={radius * 0.65 + i * parseFloat(theme.type.lineHeight[4])}
+                  style={{
+                    fontFamily: theme.type.default.fontFamily,
+                    fontSize: theme.type.fontSize[2],
+                    fill: theme.color.black,
+                    textAnchor: 'middle',
+                  }}
+                >
+                  {d}
+                </text>
+              ))}
+            </g>
+
+            <g transform={`translate(${radius * 1.6}, 0)`}>
+              <circle
+                cx={radius}
+                cy={radius}
+                r={radius}
+                fill="rgba(255, 255, 255, 0.75)"
+                stroke={theme.color.white}
+                strokeWidth="2"
+              />
+              <text
+                x={radius}
+                y={radius * 0.5}
+                style={{
+                  fontFamily: theme.type.accent.fontFamily,
+                  fontSize: theme.type.fontSize[4],
+                  fill: theme.color.black,
+                  textAnchor: 'middle',
+                }}
+              >
+                {vennData.digitalTechnology.title}
+              </text>
+              {vennData.digitalTechnology.skills.map((d, i) => (
+                <text
+                  key={d}
+                  x={radius}
+                  y={radius * 0.65 + i * parseFloat(theme.type.lineHeight[4])}
+                  style={{
+                    fontFamily: theme.type.default.fontFamily,
+                    fontSize: theme.type.fontSize[2],
+                    fill: theme.color.black,
+                    textAnchor: 'middle',
+                  }}
+                >
+                  {d}
+                </text>
+              ))}
+            </g>
+
+            <g>
+              <clipPath id="set1">
+                <circle cx={radius} cy={radius} r={255} />
+              </clipPath>
+              <clipPath id="set2" clipPath="url('#set1')">
+                <circle
+                  cx={radius}
+                  cy={radius}
+                  r={255}
+                  transform={`translate(${radius * 1.6}, 0)`}
+                />
+              </clipPath>
+              <rect
+                clipPath="url('#set2')"
+                x="0"
+                y="0"
+                width="100%"
+                height="100%"
+                fill={theme.color.pink}
+              />
+            </g>
           </g>
         </g>
       </svg>
