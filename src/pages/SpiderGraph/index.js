79ca88d0218558a4b37608c6199ce76458b41625
Dana Oira <danaoira@live.com>
2022-06-16 21:10:23 -0700

    Add responsive Venn diagram

--- src/pages/SpiderGraph/index.js
+++ src/pages/SpiderGraph/index.js
@@ -26,60 +26,159 @@ const Grid = styled.div`
 //   ]
 // }
 
+const vennData = {
+  uxDesign: {
+    title: 'User Experience Design',
+    skills: [
+      'Information Architecture',
+      'Product Design',
+      'Service Design',
+      'Interaction Design',
+      'Visual Design',
+      'UI Design',
+      'Usability Engineering',
+      'Perception, Cognition, Psychology',
+      'Human Computer Interaction',
+    ],
+  },
+  digitalTechnology: {
+    title: 'Digital Technology',
+    skills: [
+      'Software Architecture',
+      'Software Development',
+      'Software Testing',
+      'Software Development Lifecycle',
+      'UI Technology',
+      'Cloud Computing',
+      'Cyber Security',
+      'Programming Languages',
+      'Databases & Modeling',
+    ],
+  },
+}
+
 const SpiderGraph = () => {
   let ref = useRef(null)
   let [width, setWidth] = useState(720)
   let [height, setHeight] = useState(720)
+  let [radius, setRadius] = useState(256)
 
   useEffect(
     () => {
       if (!ref || !ref.current) return
       const computed = getComputedStyle(ref.current)
-      console.log(computed)
-      setWidth(parseFloat(computed.width))
-      setHeight(parseFloat(computed.height))
+      const parsedWidth = parseFloat(computed.width)
+      const parsedHeight = parseFloat(computed.height)
+      setWidth(parsedWidth)
+      setHeight(Math.min(parsedHeight, parsedWidth / 2))
+      if (width / 2 < radius) setRadius(width / 4)
     },
     [ref]
   )
 
   return (
     <Grid ref={ref}>
-      <svg width={width} height={width / 2}>
+      <svg
+        viewBox={`0 0 ${width} ${height}`}
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <rect width="100%" height="100%" fill={theme.color.pink} />
-        <g transform={`translate(${width * 0.04}, 0)`}>
-          {/* <g transform={`translate(0, 0)`}> */}
-          <circle
-            cx={width / 4}
-            cy={width / 4}
-            r={width / 4}
-            fill="rgba(255, 255, 255, 0.75)"
-            stroke={theme.color.white}
-            strokeWidth="2"
-          />
-          <circle
-            cx={width - width / 3}
-            cy={width / 4}
-            r={width / 4}
-            fill="rgba(255, 255, 255, 0.75)"
-            stroke={theme.color.white}
-            strokeWidth="2"
-          />
+        <g>
           <g>
-            <clipPath id="clip1">
-              <circle cx={width / 4} cy={width / 4} r={width / 4} />
-            </clipPath>
-            <clipPath id="clip2" clip-path="url('#clip1')">
-              <circle cx={width - width / 3} cy={width / 4} r={width / 4} />
-            </clipPath>
-            <rect
-              clip-path="url('#clip2')"
-              x="0"
-              y="0"
-              width="100%"
-              height="100%"
-              fill={theme.color.pink}
+            <circle
+              cx={radius}
+              cy={radius}
+              r={radius}
+              fill="rgba(255, 255, 255, 0.75)"
+              stroke={theme.color.white}
+              strokeWidth="2"
             />
+            <text
+              x={radius}
+              y={radius * 0.5}
+              style={{
+                fontFamily: theme.type.accent.fontFamily,
+                fontSize: theme.type.fontSize[4],
+                fill: theme.color.black,
+                textAnchor: 'middle',
+              }}
+            >
+              {vennData.uxDesign.title}
+            </text>
+            {vennData.uxDesign.skills.map((d, i) => (
+              <text
+                x={radius}
+                y={radius * 0.65 + i * parseFloat(theme.type.lineHeight[4])}
+                style={{
+                  fontFamily: theme.type.default.fontFamily,
+                  fontSize: theme.type.fontSize[2],
+                  fill: theme.color.black,
+                  textAnchor: 'middle',
+                }}
+              >
+                {d}
+              </text>
+            ))}
           </g>
+
+          <g transform={`translate(${radius * 1.6}, 0)`}>
+            <circle
+              cx={radius}
+              cy={radius}
+              r={radius}
+              fill="rgba(255, 255, 255, 0.75)"
+              stroke={theme.color.white}
+              strokeWidth="2"
+            />
+            <text
+              x={radius}
+              y={radius * 0.5}
+              style={{
+                fontFamily: theme.type.accent.fontFamily,
+                fontSize: theme.type.fontSize[4],
+                fill: theme.color.black,
+                textAnchor: 'middle',
+              }}
+            >
+              {vennData.digitalTechnology.title}
+            </text>
+            {vennData.digitalTechnology.skills.map((d, i) => (
+              <text
+                x={radius}
+                y={radius * 0.65 + i * parseFloat(theme.type.lineHeight[4])}
+                style={{
+                  fontFamily: theme.type.default.fontFamily,
+                  fontSize: theme.type.fontSize[2],
+                  fill: theme.color.black,
+                  textAnchor: 'middle',
+                }}
+              >
+                {d}
+              </text>
+            ))}
+          </g>
+        </g>
+
+        <g>
+          <clipPath id="set1">
+            <circle cx={radius} cy={radius} r={255} />
+          </clipPath>
+          <clipPath id="set2" clipPath="url('#set1')">
+            <circle
+              cx={radius}
+              cy={radius}
+              r={255}
+              transform={`translate(${radius * 1.6}, 0)`}
+            />
+          </clipPath>
+          <rect
+            clipPath="url('#set2')"
+            x="0"
+            y="0"
+            width="100%"
+            height="100%"
+            fill={theme.color.pink}
+          />
         </g>
       </svg>
     </Grid>
