78ec13668e03705c147fb003938f04652c28ce3c
Dana Oira <danaoira@live.com>
2022-06-17 01:23:23 -0700

    Update to latest dependencies minus react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import { createEpicMiddleware } from 'redux-observable'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link as RouterLink,
-} from 'react-router-dom'
-import combinedReducer from './store/combinedReducer'
-import combinedEpics from './store/combinedEpics'
+import { Route, Link as RouterLink } from 'react-router-dom'
 import { Menu, Title, Subtitle, List, Link } from './utils/style'
-import { links } from './utils'
+import { links, theme } from './utils'
 import { About, Portfolio, CareerProgression } from './pages'
-
-const epicMiddleware = createEpicMiddleware()
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-let store = createStore(
-  combinedReducer,
-  composeEnhancers(applyMiddleware(epicMiddleware))
-)
-epicMiddleware.run(combinedEpics)
 
 const Main = styled.div`
   overflow-y: scroll;
+  // background-color: ${(props) => theme.color.pink};
 
   @media (min-width: 800px) {
     padding-right: 5vw;
@@ -45,46 +29,44 @@ const ListItem = (props) => (
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-          <Menu>
-            <Title>Dana Oira Shi</Title>
-            <Subtitle>
-              Data Visualization
-              <br />
-              UI Engineer
-            </Subtitle>
-            <List>
-              <ListItem>
-                <RouterLink to="/portfolio">Portfolio</RouterLink>
-              </ListItem>
-              <ListItem>
-                <RouterLink to="/about">About</RouterLink>
-              </ListItem>
-              <ListItem href={links.linkedin}>
-                <i className="fab fa-linkedin" />
-              </ListItem>
-              <ListItem href={links.github}>
-                <i className="fab fa-github" />
-              </ListItem>
-              <ListItem href={'mailto:danaoira@live.com'}>
-                <i className="far fa-envelope" />
-              </ListItem>
-            </List>
-          </Menu>
-          <Main>
-            <Route exact path="/" component={Portfolio} />
-            <Route exact path="/about/" component={About} />
-            <Route exact path="/portfolio" component={Portfolio} />
-            <Route
-              exact
-              path="/portfolio/spider-chart"
-              component={CareerProgression}
-            />
-            {/* <Route path="/unt-ebola" component={UntEbola} /> */}
-          </Main>
-        </Router>
-      </Provider>
+      <>
+        <Menu>
+          <Title>Dana Oira Shi</Title>
+          <Subtitle>
+            Data Visualization
+            <br />
+            UI Engineer
+          </Subtitle>
+          <List>
+            <ListItem>
+              <RouterLink to="/portfolio">Portfolio</RouterLink>
+            </ListItem>
+            <ListItem>
+              <RouterLink to="/about">About</RouterLink>
+            </ListItem>
+            <ListItem href={links.linkedin}>
+              <i className="fab fa-linkedin" />
+            </ListItem>
+            <ListItem href={links.github}>
+              <i className="fab fa-github" />
+            </ListItem>
+            <ListItem href={'mailto:danaoira@live.com'}>
+              <i className="far fa-envelope" />
+            </ListItem>
+          </List>
+        </Menu>
+        <Main>
+          <Route exact path="/" component={Portfolio} />
+          <Route exact path="/about/" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route
+            exact
+            path="/portfolio/spider-chart"
+            component={CareerProgression}
+          />
+          {/* <Route path="/unt-ebola" component={UntEbola} /> */}
+        </Main>
+      </>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom'
 import './reset.css'
 import './index.css'
 import App from './App'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { createEpicMiddleware } from 'redux-observable'
+import combinedReducer from './store/combinedReducer'
+import combinedEpics from './store/combinedEpics'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const epicMiddleware = createEpicMiddleware()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+let store = createStore(
+  combinedReducer,
+  composeEnhancers(applyMiddleware(epicMiddleware))
+)
+epicMiddleware.run(combinedEpics)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
 
 serviceWorker.unregister()
